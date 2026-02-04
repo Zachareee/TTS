2d245cdce0bf1e7a9d326bc7b2cac8c40c99d263
Zachareee <zacharytanhx@yahoo.com>
2026-02-04 10:03:42 +0800

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+using namespace winrt;
+
+bool select_voice(
+    const Windows::Media::SpeechSynthesis::SpeechSynthesizer& synth)
+{
+    for (auto voice : synth.AllVoices())
+    {
+        if (to_string(voice.DisplayName()) == "Microsoft Ayumi")
+        {
+            synth.Voice(voice);
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    Windows::Media::SpeechSynthesis::SpeechSynthesizer synth{};
+    if (!select_voice(synth))
+    {
+        spdlog::error("Ayumi not found");
+        return 1;
+    }
+
+    hstring text{to_hstring("Hello world")};
+    auto stream{synth.SynthesizeTextToStreamAsync(text).get()};
+
+    Windows::Media::Playback::MediaPlayer player{};
+
+    player.SetStreamSource(stream);
+    player.Play();
+
+    std::promise<void> p;
+    std::future<void> future = p.get_future();
+
+    player.MediaEnded(
+        [&](auto...)
+        {
+            player.Close();
+            p.set_value();
+        });
+    future.get();
+}
---
+++ src/pch.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <future>
+#include <ppltasks.h>
+#include <spdlog/spdlog.h>
+
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Media.Playback.h>
+#include <winrt/Windows.Media.SpeechSynthesis.h>
